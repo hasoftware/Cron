8d8551c5a5b52fc572e6b01efcb33f4144eb8378
HASOFTWARE <109876747+hasoftware@users.noreply.github.com>
2024-04-11 04:28:14 +0700

    Update cron.sh

--- cron.sh
+++ cron.sh
@@ -1,24 +1,7 @@
 #!/bin/bash
 
-# Hàm truy cập và ghi log cho một link
-function truy_cap_ghi_log {
-  link=$1
-  echo "$(date +"%Y-%m-%d %H:%M:%S") - Truy cập link $link" >> cronmain.log
-  curl -s $link >> cronmain.log
-}
-
-# Truy cập link 1 và ghi log
-truy_cap_ghi_log https://muafb88.com/cron/bank.php
-
-# Truy cập link 2 và ghi log
-truy_cap_ghi_log https://muafb.net/cron/bank.php
-
-# Chờ 5 giây
-sleep 5
-
-# Lặp lại
 while true; do
-  truy_cap_ghi_log https://muafb88.com/cron/bank.php
-  truy_cap_ghi_log https://muafb.net/cron/bank.php
-  sleep 5
+    wget -qO- https://muafb88.com/cron/bank.php >/dev/null 2>&1
+    wget -qO- https://muafb.net/cron/bank.php >/dev/null 2>&1
+    sleep 3
 done
